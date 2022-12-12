42abe86a7347c3e8210dece159320232256caf6e
“Bethany <Bethanygellington@gmail.com>
2022-12-12 13:50:00 +0800

    finish code for exercises in lesson 1 and 2

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,20 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string) {
+      let errorMsg = '';
+      if(newTitle === "" || newTitle === " "){
+         errorMsg = "please enter a movie title.";
+      } else if (this.movies.includes(newTitle)) {
+         errorMsg = `${newTitle} is already in your movie list.`;
+      } else {
+         this.movies.push(newTitle);
+      }
+      return errorMsg;
+   }
 }
+
+// if(!this.movies.includes(newTitle) && newTitle !== ' '){
+      //    this.movies.push(newTitle)
+      //  }
